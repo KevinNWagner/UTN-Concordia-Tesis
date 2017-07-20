102d92b2c9f0022d3a9c763024b99fe2c78f0390
Kevin Wagner <kevinwe85@gmail.com>
2017-07-20 19:10:22 -0300

    cambio de rutas para inspector

--- adminC/app/Reporte.php
+++ adminC/app/Reporte.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reporte extends Model
 {
-    //
+    protected $table='reportes_inspectores';
+    protected $primaryKey='idRendicionesBoleteras';
+
+    public $timestamps=false;
+
+    protected $fillable=[
+            'recaudacion',
+            'fecha',
+            'observacionAdmin' ,
+            'boletoPrimera',
+            'beletoSegunda',
+            'boletoEscolar' ,
+            'boletoSecundario',
+            'boletoJubilado' ,
+            'boleteoCombinado' ,
+            'Empleados_idEmpleados' ,
+            'Colectivos_idColectivos',            
+    ];
+     protected $guarded = [
+
+    ]; 
+
 }
